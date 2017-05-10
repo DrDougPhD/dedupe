bb962b4a0f25b751c951360745d03d0e346e465a
Doug McGeehan <djmvfb@mst.edu>
2017-05-10 07:39:20 -0500

    added comments to the function that filters out filesize-based singleton file groups

--- dedupe/utils.py
+++ dedupe/utils.py
@@ -4,9 +4,11 @@ import pprint
 logger = logging.getLogger(__name__)
 
 def filter_singletons(partitions):
+    # identify filesizes that correspond to one or no files
     sizes_to_filter = filter(lambda key: len(partitions[key]) <= 1,
                              partitions)
 
+    # delete these singleton filesizes from the set
     for size in list(sizes_to_filter):
         del partitions[size]
 
